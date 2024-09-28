7eccb937fe3b1b9ff7ec8ae456e694f4d78b0a38
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-09-28 21:36:34 +0700

    feat: use 3 classes

--- tools/demo.py
+++ tools/demo.py
@@ -10,9 +10,9 @@ from mmseg.apis import init_model, inference_model, show_result_pyplot, MMSegInf
 
 def demo():
     """Demo script."""
-    config_path = "work_dirs/unet-s5-d16_deeplabv3_breast-cancer_1500x1188_512x512/unet-s5-d16_deeplabv3_breast-cancer.py"
+    config_path = "work_dirs/unet-s5-d16_deeplabv3_breast-cancer/unet-s5-d16_deeplabv3_breast-cancer.py"
     checkpoint_path = (
-        "work_dirs/unet-s5-d16_deeplabv3_breast-cancer_1500x1188_512x512/iter_10000.pth"
+        "work_dirs/unet-s5-d16_deeplabv3_breast-cancer/iter_28000.pth"
     )
 
     model = init_model(config_path, checkpoint_path, "cpu")
@@ -43,23 +43,23 @@ def demo2():
 
     config_path = "work_dirs/unet-s5-d16_deeplabv3_breast-cancer/unet-s5-d16_deeplabv3_breast-cancer.py"
     checkpoint_path = (
-        "work_dirs/unet-s5-d16_deeplabv3_breast-cancer/iter_40000.pth"
+        "work_dirs/unet-s5-d16_deeplabv3_breast-cancer/iter_28000.pth"
     )
 
     inferencer = MMSegInferencer(
         config_path,
         checkpoint_path,
         device="cuda:0",
-        classes=["background", "tumor"],
-        palette=[[0, 0, 0], [255, 255, 255]],
+        classes=["background", "benign", "malignant"],
+        palette=[[0, 0, 0], [100, 100, 100], [200, 200, 200]],
     )
 
     # imgs_path = "data/bc-dataset/tmp/final/images/val"
-    imgs_path = "data/bc-dataset/tmp/data_final/images/val"
+    imgs_path = "data/bc-dataset/tmp/data_final_cls/images/val"
     imgs_path = Path(imgs_path)
 
     # output_dir = "tmp/breast-cancer-14-dice-57-40k"
-    output_dir = "tmp/breast-cancer-640x640-2-val"
+    output_dir = "tmp/breast-cancer-640x640-end2end"
     output_dir = Path(output_dir)
 
     for i, img_path in tqdm(enumerate(sorted(list(imgs_path.glob("*.jpg"))))):
